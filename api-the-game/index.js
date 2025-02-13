a10c4d289052ffd8171325ef5d1d1df578f279e7
Arthur Fudali <99662198+arthurfudali@users.noreply.github.com>
2025-02-12 22:23:17 -0300

    Update index.js

--- api-the-game/index.js
+++ api-the-game/index.js
@@ -1,16 +1,22 @@
-import express from 'express'
-const app = express()
+import express from "express";
+const app = express();
 
 // configuração do express
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Rota principal
+app.get("/", (req, res) => {
+  res.send("API iniciada com sucesso!");
+});
+
 
 // Iniciando o servidor
-const port = 4000
-app.listen(port, (error)=>{
-    if(error){
-        console.log(error)
-    } else{
-        console.log(`API rodando em http://localhost:${port}`)
-    }
-})
+const port = 4000;
+app.listen(port, (error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(`API rodando em http://localhost:${port}`);
+  }
+});
