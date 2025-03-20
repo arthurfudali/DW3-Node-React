bffe49cbe6e9a57651b2f5a3421351a8bec4a6c2
Arthur Fudali <99662198+arthurfudali@users.noreply.github.com>
2025-03-20 19:49:44 -0300

    correção da conexao

--- api-the-game-mongodb-atlas/config/ds-connection.js
+++ api-the-game-mongodb-atlas/config/ds-connection.js
@@ -1,13 +1,13 @@
-import mongoose, { connect } from "mongoose";
-import dotenv from "dotenv";
+import mongoose from "mongoose";
+import dotenv from "dotenv"; // Importando o dotenv para acessar as variáveis de ambiente de forma segura na pasta .env (Senha do banco de dados)
 dotenv.config();
-const connnect = () =>{
+const connect = () =>{
     mongoose.connect(
         `mongodb+srv://arthurfudali:${process.env.DB_PASSWORD}@cluster0.79onb.mongodb.net/api-the-game?retryWrites=true&w=majority&appName=Cluster0`
     );
     const connection = mongoose.connection;
     connection.on("error", () => {
-        console.log("Erro ao conectar ao servidor")
+        console.log("Erro ao conectar ao servidor");
     });
     connection.on("open", () => {
         console.log("Conectado ao servidor")
